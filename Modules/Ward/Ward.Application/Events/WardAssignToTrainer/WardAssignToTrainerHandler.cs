c0d6cd6cfcad4742a2336ef4408cd402de0ceafe
poglodek <pawelogll@gmail.com>
2024-08-07 21:51:36 +0200

    Ward fixing

--- Modules/Ward/Ward.Application/Events/WardAssignToTrainer/WardAssignToTrainerHandler.cs
+++ Modules/Ward/Ward.Application/Events/WardAssignToTrainer/WardAssignToTrainerHandler.cs
@@ -15,6 +15,12 @@ public class WardAssignToTrainerHandler : INotificationHandler<WardAssignToTrain
     public async Task Handle(WardAssignToTrainerCommand notification, CancellationToken cancellationToken)
     {
         var ward = await _wardRepository.GetWardById(notification.WardId, cancellationToken);
-        if(ward is n)
+        if (ward is null)
+        {
+            ward = new Domain.Entity.Ward(notification.WardId, notification.TrainerId);
+            await _wardRepository.AddAsync(ward, cancellationToken);
+        }
+        
+        ward.AssignToTrainer(notification.TrainerId);
     }
 }
