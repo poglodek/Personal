367da471ca08a36eef7ca7ffadd49d76824155dc
poglodek <pawelogll@gmail.com>
2024-08-27 22:16:45 +0200

    WASM to SS, adding few pages

---
+++ Personal.Blazor/Blazor/Components/Pages/Auth/SignIn.razor.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor.Components.Pages.Auth;
+
+public partial class SignIn : ComponentBase
+{
+}
---
+++ Personal.Blazor/Blazor/Program.cs
@@ -0,0 +1,25 @@
+using Blazor.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+await app.RunAsync();
